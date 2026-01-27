51d663bde9df25f1314c58a152d0f41ebc784578
Shreeram N S <shreeram.ns@bounteous.com>
2026-01-27 15:26:43 +0530

    Delete backend/src/main/java/com/company/turbohire/backend/repos directory

--- backend/src/main/java/com/company/turbohire/backend/repos/CandidateProfileRepository.java
+++
@@ -1,7 +0,0 @@
-package com.company.turbohire.backend.repos;
-
-import com.company.turbohire.backend.entity.CandidateProfile;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CandidateProfileRepository extends JpaRepository<CandidateProfile,Long> {
-}
--- backend/src/main/java/com/company/turbohire/backend/repos/ResumeRepository.java
+++
@@ -1,7 +0,0 @@
-package com.company.turbohire.backend.repos;
-
-import com.company.turbohire.backend.entity.Resume;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ResumeRepository extends JpaRepository<Resume,Long> {
-}
