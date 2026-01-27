8cdd430524e0b7ffc4b9c2068609f44ad9498982
deeptik-debug <deepti.kademani@bounteous.com>
2026-01-27 00:41:36 -0800

    Add Interview Repository File

---
+++ backend/src/main/java/com/company/turbohire/backend/repository/InterviewAssignmentRepository.java
@@ -0,0 +1,7 @@
+package com.company.turbohire.backend.repository;
+
+import com.company.turbohire.backend.entity.InterviewAssignment;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface InterviewAssignmentRepository extends JpaRepository<InterviewAssignment,Long> {
+}
